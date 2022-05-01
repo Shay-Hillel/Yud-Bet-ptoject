acc546bb910025a0bed7a3578ee0b55cd3ff0154
Shay-Hillel <hillelshay@gmail.com>
2022-05-01 14:50:40 +0300

    in main activity : int hadas

--- app/src/main/java/com/example/shaysgame/MainActivity.java
+++ app/src/main/java/com/example/shaysgame/MainActivity.java
@@ -23,7 +23,6 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     static Player nowPlayingInThisPhone;
-int x;
     Intent musicIntent;
 
     LowBatteryReceiver lowBatteryReceiver = new LowBatteryReceiver();
