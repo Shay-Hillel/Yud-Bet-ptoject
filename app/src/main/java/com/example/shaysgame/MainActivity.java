e91de12f40042a65968058dd2945d86f86e77e75
Shay-Hillel <hillelshay@gmail.com>
2022-05-01 14:44:13 +0300

    initial commit

--- app/src/main/java/com/example/shaysgame/MainActivity.java
+++ app/src/main/java/com/example/shaysgame/MainActivity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     static Player nowPlayingInThisPhone;
-
+int x;
     Intent musicIntent;
 
     LowBatteryReceiver lowBatteryReceiver = new LowBatteryReceiver();
